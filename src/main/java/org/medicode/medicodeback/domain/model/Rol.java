fd3059c2e3166c69dd66a7d7e330ab786efb666b
Ariel <arielcaballeroventura@gmail.com>
2026-08-06 18:57:08 -0600

    feat: creacion del modelo Rol en domain y JpaRolEntity  en persistence

---
+++ src/main/java/org/medicode/medicodeback/domain/model/Rol.java
@@ -0,0 +1,16 @@
+package org.medicode.medicodeback.domain.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Rol {
+    private Integer idRol;
+    private String name;
+}
---
+++ src/main/java/org/medicode/medicodeback/infrastructure/adapters/out/persistence/JpaRolEntity.java
@@ -0,0 +1,24 @@
+package org.medicode.medicodeback.infrastructure.adapters.out.persistence;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "rol")
+public class JpaRolEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol")
+    private Integer idRol;
+
+    @Column(name = "name", length = 50, nullable = false, unique = true)
+    private String name;
+}
